522e97cf6691424cbd2f1d42796883973b22507d
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-25 18:34:24 +0530

    "check_style_score -> 23 of 24 passed. style score: 0/1 "

---
+++ ADS-1 assignments/M2/CodeCamp_Percolation/Solution.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
+class Percolation {
+	/**
+	 * declaration of wqu.
+	 */
+	private WeightedQuickUnionUF wqu;
+	/**
+	 * private declaration of n.
+	 */
+	private int n;
+	/**
+	 * declaration of size.
+	 */
+	private int size;
+	/**
+	 * declaration of top.
+	 */
+	private int top;
+	/**
+	 * declaration of bottom.
+	 */
+	private int bottom;
+	private int count;
+	/**
+	 * declaration of boolean array.
+	 */
+	boolean[] connected;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n1    The n 1
+	 */
+	public Percolation(final int n1) {
+		this.n = n1;
+		this.size = n1 * n1;
+		this.top = size;
+		this.bottom = size + 1;
+		this.count = 0;
+		wqu = new WeightedQuickUnionUF(size + 2);
+		connected = new boolean[size];
+		for (int i = 0; i < n; i++) {
+			wqu.union(top, i);
+			wqu.union(bottom, size - i - 1);
+		}
+	}
+	/**
+	 * converts to 1D array.
+	 *
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	private int convert(final int i, final int j) {
+//since we take size as 0 to n-1, we decrement one value from rows and columns.
+		return n * (i - 1) + (j - 1);
+	}
+	/**
+	 * Connects open sites(== full site).
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 */
+	private void connectOpenSites(final int row, final int col) {
+		if (connected[col] && !wqu.connected(row, col)) {
+			wqu.union(row, col);
+		}
+	}
+	/**
+	 * opens the blocked sites.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 */
+	public void open(final int row, final int col) {
+		// int count = 0;
+		int index = convert(row, col);
+		connected[index] = true;
+		count++;
+		int toprow = index - n;
+		int bottomrow = index + n;
+		if (n == 1) {
+			connectOpenSites(toprow, index);
+			connectOpenSites(bottomrow, index);
+		}
+		if (bottomrow < size) { //bottom
+			connectOpenSites(index, bottomrow);
+		}
+		if (toprow >= 0) { //top
+			connectOpenSites(index, toprow);
+		}
+		if (col == 1) { //left
+			if (col != n) {
+				connectOpenSites(index, index + 1);
+			}
+			return;
+		}
+		if (col == n) { //right
+			connectOpenSites(index, index - 1);
+			return;
+		}
+		connectOpenSites(index, index + 1);
+		connectOpenSites(index, index - 1);
+	}
+	/**
+	 * Determines if it is an open site.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 *
+	 * @return     True if open, False otherwise.
+	 */
+	public boolean isOpen(final int row, final int col) {
+		return connected[convert(row, col)];
+	}
+	// public boolean isFull(int row, int col) {
+	// 	return false;
+	// }
+	public int numberOfOpenSites() {
+		return count;
+	}
+	public boolean percolates() {
+		return wqu.connected(top, bottom);
+	}
+}
+/**
+ * class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//not used
+	}
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		// WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		Percolation p = new Percolation(n);
+		while (sc.hasNext()) {
+			String[] tokens = sc.nextLine().split(" ");
+			p.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+		}
+		System.out.println(p.percolates() && p.numberOfOpenSites() != 0);
+	}
+}
