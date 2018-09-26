480e555975f1c4e6efd4ab6505d97bb22686236d
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-26 19:18:48 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1 assignments/M3/Sorted_Array/Solution.java
+++ ADS-1 assignments/M3/Sorted_Array/Solution.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
 class Sortedarray {
 	public boolean containsKey(ArrayList<Integer> array, int key) {
 		for (int i = 0; i < array.size(); i++) {
@@ -22,6 +23,8 @@ class Sortedarray {
 		ArrayList<Integer> result = new ArrayList<>();
 		// int array1[];
 		// int array2[];
+		// System.out.println(Arrays.toString(array1));
+		// System.out.println(Arrays.toString(array2));
 		if (array1.length > array2.length) {
 			for (int i = 0; i < array1.length; i++) {
 				if (containsKey(array2, array1[i])) {
@@ -30,10 +33,15 @@ class Sortedarray {
 					}
 				}
 			}
-		} for (int i = 0; i < array2.length; i++) {
-			if (containsKey(array1, array2[i])) {
-				if (!containsKey(result, array2[i])) {
-					result.add(array2[i]);
+		} else {
+			for (int i = 0; i < array2.length; i++) {
+				System.out.println("38" + array2[i]);
+				if (containsKey(array1, array2[i])) {
+					System.out.println("40" + array2[i]);
+					if (!containsKey(result, array2[i])) {
+						System.out.println("42" + array2[i]);
+						result.add(array2[i]);
+					}
 				}
 			}
 		}
@@ -47,21 +55,29 @@ class Solution {
 	}
 	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
-		Sortedarray s = new Sortedarray();
+		// Sortedarray s = new Sortedarray();
 		int n1 = Integer.parseInt(input.nextLine());
 		int n2 = Integer.parseInt(input.nextLine());
 		String[] array1temp = input.nextLine().split(",");
 		String[] array2temp = input.nextLine().split(",");
-		ArrayList<Integer> array = new ArrayList<>();
+		// ArrayList<Integer> array = new ArrayList<>();
 		int[] array1 = new int[n1];
 		int[] array2 = new int[n2];
+		// for (int i = 0; i < n1; i++) {
+		// 	array1[i] = Integer.parseInt(array1temp[i]);
+		// }
+		// for (int i = 0; i < n2; i++) {
+		// 	array2[i] = Integer.parseInt(array2temp[i]);
+		// }
+		int[] array = new int[n1 + n2];
 		for (int i = 0; i < n1; i++) {
-			array1[i] = Integer.parseInt(array1temp[i]);
+			array[i] = Integer.parseInt(array1temp[i]);
 		}
-		for (int i = 0; i < n2; i++) {
-			array2[i] = Integer.parseInt(array2temp[i]);
+		for (int i = n1; i < n1+n2; i++) {
+			array[i] = Integer.parseInt(array2temp[i-n1]);
 		}
-		System.out.println(s.sortedArray(array1, array2));
+		// System.out.println(s.sortedArray(array1, array2));
+		System.out.println(Arrays.toString(array));
 	}
 }
 
