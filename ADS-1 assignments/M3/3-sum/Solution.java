2b045bc5677760f94dc24e4ed22b07d37770b29a
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-26 21:18:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS-1 assignments/M3/3-sum/Solution.java
+++ ADS-1 assignments/M3/3-sum/Solution.java
@@ -23,17 +23,17 @@ class ThreeSum {
     /**
      * to find sum of three elements.
      *
-     * @param      array  The array
+     * @param      array1  The array
      *
      * @return     { description_of_the_return_value }
      */
-    public int threeSum(final int[] array) {
+    public int threeSum(final int[] array1) {
         BinarySearch bs = new BinarySearch();
-        Arrays.sort(array);
+        Arrays.sort(array1);
         int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                int result = bs.binarySearch(array, -(array[i] + array[j]));
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = i + 1; j < array1.length; j++) {
+                int result = bs.binarySearch(array1, -(array1[i] + array1[j]));
                 if (result > j) {
                     count++;
                 }
