26a2293c44467bfd8a7b2d42389fc0d02bda47d2
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-27 18:45:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 assignments/M4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1 assignments/M4/4.1 Balanced Paranthesis/Solution.java
@@ -1,17 +1,36 @@
+/**
+ * Balanced Paranthesis.
+ * @author Swapnika Vakacharla.
+ */
 import java.util.Scanner;
+/**
+ * class for stack.
+ */
 class Stack {
-
+	/**
+	 * Class for node.
+	 */
 	class Node {
 		char item;
 		Node next;
 	}
 	Node head = null;
-	public void push(char p) {
+	/**
+	 * push the elements.
+	 *
+	 * @param      p     { parameter_description }
+	 */
+	public void push(final char p) {
 		Node lol = head;
 		head = new Node();
 		head.item = p;
 		head.next = lol;
 	}
+	/**
+	 * pop the elements.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public char pop() {
 		if (head == null) {
 			return 'e';
@@ -26,6 +45,11 @@ class Stack {
 		return r;
 		// }
 	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		String str = "";
 		Node fun = head;
@@ -36,22 +60,38 @@ class Stack {
 		return str;
 	}
 }
+/**
+ * Class for balanced paranthesis.
+ */
 class BalancedParanthesis {
+	/**
+	 * Constructs the object.
+	 */
 	BalancedParanthesis() {
 		//not used
 	}
-	public boolean balance(String str) {
+	/**
+	 * check the balance of paranthesis.
+	 *
+	 * @param      str   The string
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public boolean balance(final String str) {
 		Stack s = new Stack();
 		for (int i = 0; i < str.length(); i++) {
 			// if (head == null) {
 			// 	return true;
 			// }
 			// System.out.println(s + " " + str.charAt(i));
-			if (str.charAt(i) == '[' || str.charAt(i) == '(' || str.charAt(i) == '{') {
+			if (str.charAt(i) == '[' || str.charAt(i) == '('
+				|| str.charAt(i) == '{') {
 				s.push(str.charAt(i));
 			} else {
 				char ch = s.pop();
-				if ((ch == '(' && str.charAt(i) == ')') || (ch == '[' && str.charAt(i) == ']') || (ch == '{' && str.charAt(i) == '}')) {
+				if ((ch == '(' && str.charAt(i) == ')') || (ch == '['
+					&& str.charAt(i) == ']') || (ch == '{'
+					&& str.charAt(i) == '}')) {
 					// ch = s.pop();
 					// System.out.println(ch + "39"+ str.charAt(i));
 					continue;
@@ -67,7 +107,7 @@ public final class Solution {
 	Solution() {
 		//not used.
 	}
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = Integer.parseInt(scan.nextLine());
 		String[] strarray = new String[n];
