b6dd6210f62f61e1ef26617ad3bc5d2ca60d0cf9
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-27 18:20:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1 assignments/M4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+class Stack {
+
+	class Node {
+		char item;
+		Node next;
+	}
+	private Node head = null;
+	public void push(char p) {
+		Node lol = head;
+		head = new Node();
+		head.item = p;
+		head.next = lol;
+	}
+	public char pop() {
+		if (head == null) {
+			return 'e';
+		}
+		if (head.next == null) {
+			char r = head.item;
+			head = null;
+			return r;
+		} else {
+			char r = head.item;
+			head = head.next;
+			return r;
+		}
+	}
+	public String toString() {
+		String str = "";
+		Node fun = head;
+		while (fun != null) {
+			str += fun.item;
+			fun = fun.next;
+		}
+		return str;
+	}
+}
+class BalancedParanthesis {
+	BalancedParanthesis() {
+		//not used
+	}
+	public boolean balance(String str) {
+		Stack s = new Stack();
+		for (int i = 0; i < str.length(); i++) {
+			// if (head == null) {
+			// 	return true;
+			// }
+			System.out.println(s);
+			if (str.charAt(i) == '[' || str.charAt(i) == '(' || str.charAt(i) == '{') {
+				s.push(str.charAt(i));
+			} else {
+				char ch = s.pop();
+				if ((ch == '(' && str.charAt(i) == ')') || (ch == '[' && str.charAt(i) == ']') || (ch == '{' && str.charAt(i) == '}')) {
+					ch = s.pop();
+					// System.out.println(ch + "39"+ str.charAt(i));
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+public final class Solution {
+	Solution() {
+		//not used.
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		String[] strarray = new String[n];
+		for (int i = 0; i < n; i++) {
+			strarray[i] = scan.nextLine();
+		}
+		BalancedParanthesis bp = new BalancedParanthesis();
+		for (int i = 0; i < n; i++) {
+			System.out.println(bp.balance(strarray[i]));
+		}
+    }
+}
