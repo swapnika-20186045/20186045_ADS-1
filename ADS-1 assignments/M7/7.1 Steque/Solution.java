818a21a61b7b3000658d6158fdc08e61745d7587
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-01 12:30:32 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS-1 assignments/M7/7.1 Steque/Solution.java
@@ -0,0 +1,103 @@
+/**
+ * Steque.
+ * @author Swapnika Vakacharla.
+ */
+import java.util.Scanner;
+class Steque {
+	public Node head;
+	public Node tail;
+	int size;  
+    private class Node {
+        private int data;
+        private Node next;
+        int size = 0;
+    }
+    Steque() {
+    	head = null;
+        tail = null;
+    }
+    public void enqueue(final int data) {
+    	Node temp = new Node();
+		if (size == 0) {
+			head = temp;
+			tail = temp;
+			size++;
+			return;
+		}
+		tail.next = temp;
+		tail = temp;
+		size++;
+    }
+    public void push(final int data) {
+    	Node newnode = new Node();
+        newnode.next = head;
+        head = newnode;
+        size++;
+    }
+    public int pop() {
+    	int item = head.data;
+        head = head.next;
+        size--;
+        return item;
+    }
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null || tail == null;
+    }
+
+    public String display() {
+        if (size != 0) {
+            String str = "";
+            Node temp = head;
+            while (temp != null) {
+                str += temp.data + ", ";
+                temp = temp.next;
+            }
+            return str.substring(0, str.length() - 2);
+        }
+        return "";
+    }
+}
+class Solution {
+	Solution() {
+		//empty constructor.
+	}
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < n; i++) {
+            Steque sq = new Steque();
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] tokens = line.split(" ");
+                switch (tokens[0]) {
+                    case "push":
+                        sq.push(Integer.parseInt(tokens[1]));
+                        System.out.println(sq.display());
+                    break;
+                    case "pop":
+                        sq.pop();
+                        if (sq.isEmpty()) {
+                            System.out.println("steque is empty.");
+                        } else {
+                            System.out.println(sq.display());
+                        }
+                    break;
+                    case "enqueue":
+                        sq.enqueue(Integer.parseInt(tokens[1]));
+                        System.out.println(sq.display());
+                    break;
+                    default:
+                    break;
+                }
+                if (tokens[0].equals("")) {
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
+}
