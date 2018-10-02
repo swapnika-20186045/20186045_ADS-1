d812479e5b91cbe9927fcccf753932dc199d95da
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-02 17:22:23 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1 assignments/M8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1 assignments/M8/8.1 Team Ranking With Insertion/Solution.java
@@ -8,182 +8,189 @@ import java.util.Arrays;
  * Class for tournament.
  */
 class Tournament implements Comparable {
-	/**
-	 * declaration of teamname.
-	 */
-	private String teamname;
-	/**
-	 * declaration of wins.
-	 */
-	private int wins;
-	/**
-	 * declaration of losses.
-	 */
-	private int losses;
-	/**
-	 * declaration of draws.
-	 */
-	private int draws;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      teamname    The teamname
-	 * @param      wins    The wins
-	 * @param      losses  The losses
-	 * @param      draws   The draws
-	 */
-	public Tournament(String teamnamee, int winss, int lossess, int drawss) {
-		this.teamname = teamnamee;
-		this.wins = winss;
-		this.losses = lossess;
-		this.draws = drawss;
-	}
-	/**
-	 * Gets the teamname.
-	 *
-	 * @return     The teamname.
-	 */
-	public String getTeamName() {
-		return teamname;
-	}
-	/**
-	 * Gets the wins.
-	 *
-	 * @return     The wins.
-	 */
-	public int getWins() {
-		return wins;
-	}
-	/**
-	 * Gets the losses.
-	 *
-	 * @return     The losses.
-	 */
-	public int getLosses() {
-		return losses;
-	}
-	/**
-	 * Gets the draws.
-	 *
-	 * @return     The draws.
-	 */
-	public int getDraws() {
-		return draws;
-	}
-	/**
-	 * compares the wins, losses and draws.
-	 *
-	 * @param      object  The object
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int compareTo(Object object) {
-		Tournament that = (Tournament) object;
-		if (this.getWins() > that.getWins()) {
-			return 1;
-		} else if (this.getWins() < that.getWins()) {
-			return -1;
-		}
-		if (this.getLosses() > that.getLosses()) {
-			return -1;
-		} else if (this.getLosses() < that.getLosses()) {
-			return 1;
-		}
-		if (this.getDraws() > that.getDraws()) {
-			return 1;
-		} else if (this.getDraws() < that.getDraws()) {
-			return -1;
-		}
-		return 0;
-	}
+    /**
+     * declaration of teamname.
+     */
+    private String teamname;
+    /**
+     * declaration of wins.
+     */
+    private int wins;
+    /**
+     * declaration of losses.
+     */
+    private int losses;
+    /**
+     * declaration of draws.
+     */
+    private int draws;
+    /**
+     * Constructs the object.
+     *
+     * @param      teamnamee    The teamname
+     * @param      winss    The wins
+     * @param      lossess  The losses
+     * @param      drawss   The draws
+     */
+    public Tournament(final String teamnamee, final int winss,
+                      final int lossess, final int drawss) {
+        this.teamname = teamnamee;
+        this.wins = winss;
+        this.losses = lossess;
+        this.draws = drawss;
+    }
+    /**
+     * Gets the teamname.
+     *
+     * @return     The teamname.
+     */
+    public String getTeamName() {
+        return teamname;
+    }
+    /**
+     * Gets the wins.
+     *
+     * @return     The wins.
+     */
+    public int getWins() {
+        return wins;
+    }
+    /**
+     * Gets the losses.
+     *
+     * @return     The losses.
+     */
+    public int getLosses() {
+        return losses;
+    }
+    /**
+     * Gets the draws.
+     *
+     * @return     The draws.
+     */
+    public int getDraws() {
+        return draws;
+    }
+    /**
+     * compares the wins, losses and draws.
+     *
+     * @param      object  The object
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Object object) {
+        Tournament that = (Tournament) object;
+        if (this.getWins() > that.getWins()) {
+            return 1;
+        } else if (this.getWins() < that.getWins()) {
+            return -1;
+        }
+        if (this.getLosses() > that.getLosses()) {
+            return -1;
+        } else if (this.getLosses() < that.getLosses()) {
+            return 1;
+        }
+        if (this.getDraws() > that.getDraws()) {
+            return 1;
+        } else if (this.getDraws() < that.getDraws()) {
+            return -1;
+        }
+        return 0;
+    }
 }
 /**
  * Class for insertion.
  */
 class Insertion {
-	/**
-	 * declaration of array of tournaments.
-	 */
-	private Tournament[] tournaments;
-	/**
-	 * declaration of size.
-	 */
-	private int size;
-	/**
-	 * Constructs the object.
-	 */
-	public Insertion() {
-		tournaments = new Tournament[10];
-		size = 0;
-	}
-	/**
-	 * Adds a tournament.
-	 *
-	 * @param      t     { parameter_description }
-	 */
-	public void addTournament(Tournament t) {
-		tournaments[size++] = t;
-	}
-	/**
-	 * sort the teams.
-	 */
-	public void sort() {
-		for (int i = 0; i < size; i++) {
-			for (int j = i; j > 0; j--) {
-				if (tournaments[j].compareTo(tournaments[j - 1]) == 1) {
-					exchange(tournaments, j, j-1);
-				} else {
-					break;
-				}
-			}
-		}
-	}
-	/**
-	 * swaps the teams in sorting order.
-	 *
-	 * @param      arr     { parameter_description }
-	 * @param      i     { parameter_description }
-	 * @param      j     { parameter_description }
-	 */
-	public void exchange(Tournament[] arr, int i, int j) {
-		Tournament temp = tournaments[i];
-		tournaments[i] = tournaments[j];
-		tournaments[j] = temp;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		String str = "";
-		int i = 0;
-		for (i = 0; i < size - 1; i++) {
-		str += tournaments[i].getTeamName() + ",";	
-		}
-		str += tournaments[i].getTeamName();
-		return str;
-	}
+    /**
+     * declaration of array of tournaments.
+     */
+    private Tournament[] tournaments;
+    /**
+     * declaration of size.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     */
+    public Insertion() {
+        tournaments = new Tournament[10];
+        size = 0;
+    }
+    /**
+     * Adds a tournament.
+     *
+     * @param      t     { parameter_description }
+     */
+    public void addTournament(final Tournament t) {
+        tournaments[size++] = t;
+    }
+    /**
+     * sort the teams.
+     */
+    public void sort() {
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j > 0; j--) {
+                if (tournaments[j].compareTo(tournaments[j - 1]) == 1) {
+                    exchange(tournaments, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    /**
+     * swaps the teams in sorting order.
+     *
+     * @param      arr     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    public void exchange(final Tournament[] arr, final int i, final int j) {
+        Tournament temp = tournaments[i];
+        tournaments[i] = tournaments[j];
+        tournaments[j] = temp;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        int i = 0;
+        for (i = 0; i < size - 1; i++) {
+            str += tournaments[i].getTeamName() + ",";
+        }
+        str += tournaments[i].getTeamName();
+        return str;
+    }
 }
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Client program.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Insertion list = new Insertion();
-		while (scan.hasNext()) {
-			String[] s = scan.nextLine().split(",");
-			list.addTournament(new Tournament(s[0],
-				Integer.parseInt(s[1]), Integer.parseInt(s[2]),
-				Integer.parseInt(s[3])));
-		}
-		list.sort();
-		System.out.println(list);
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty constructor
+    }
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Insertion list = new Insertion();
+        while (scan.hasNext()) {
+            String[] s = scan.nextLine().split(",");
+            list.addTournament(new Tournament(s[0],
+                Integer.parseInt(s[1]), Integer.parseInt(s[2]),
+                Integer.parseInt(s[3])));
+        }
+        list.sort();
+        System.out.println(list);
+    }
 }
