997e9ebb0996d5f1e9da37d5d166046682d0e0ec
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-06 12:59:14 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/M12 (exam 2)/Assignment-1/Solution.java
+++ ADS-1 assignments/M12 (exam 2)/Assignment-1/Solution.java
@@ -163,21 +163,21 @@ public final class Solution {
         System.out.println();
         Fillmerit(students, vacancies, unreserved, bc, sc, st);
     }
-    public static void Fillmerit(Studentinfo[] arr, int vac, int unres, int bcs, int scs, int sts) {
+    public static void Fillmerit(Studentinfo[] arr, int vac, int unres, int bc, int sc, int st) {
         for (int i = 0; i < arr.length && vac > 0; i++) {
             if (unres > 0) {
                 System.out.println(arr[i].getStudentName() + "," + arr[i].getTotal() + "," + arr[i].getReservation());
                 unres--;
             } else if (arr[i].getReservation().equals("BC") || arr[i].getReservation().equals("SC") || arr[i].getReservation().equals("ST")) {
-                if (arr[i].getReservation().equals("BC") && bcs > 0) {
+                if (arr[i].getReservation().equals("BC") && bc > 0) {
                     System.out.println(arr[i].getStudentName() + "," + arr[i].getTotal() + "," + arr[i].getReservation());
-                    bcs--;
-                } else if (arr[i].getReservation().equals("SC") && scs > 0) {
+                    bc--;
+                } else if (arr[i].getReservation().equals("SC") && sc > 0) {
                     System.out.println(arr[i].getStudentName() + "," + arr[i].getTotal() + "," + arr[i].getReservation());
-                    scs--;
-                } else if (arr[i].getReservation().equals("ST") && sts > 0) {
+                    sc--;
+                } else if (arr[i].getReservation().equals("ST") && st > 0) {
                     System.out.println(arr[i].getStudentName() + "," + arr[i].getTotal() + "," + arr[i].getReservation());
-                    sts--;
+                    st--;
                 }
             }
         }
