62688c136a8541363aedc333b8ca69bfe7497f5e
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-10 09:22:23 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1 assignments/M14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1 assignments/M14/14.1 Dynamic Median Finding/Solution.java
@@ -1,103 +1,10 @@
 /**
  * Dynamic median finding.
- * @author exchangenika Vakacharla.
+ * @author swapnika Vakacharla.
  */
 import java.util.Scanner;
 /**
- * Class for heapsort.
- */
-class Heapsort {
-    /**
-     * declaration of array.
-     */
-    private int[] array;
-    /**
-     * declaration of size.
-     */
-    private int size;
-    /**
-     * Constructs the object.
-     *
-     * @param      n     { parameter_description }
-     */
-    Heapsort(final int n) {
-        array = new int[n];
-        size = 0;
-    }
-    /**
-     * sort function.
-     */
-    public void sort() {
-        for (int i = (size / 2) - 1; i >= 0; i--) {
-            heapify(size, i);
-        }
-        for (int i = size - 1; i >= 0; i--) {
-            exchange(0, i);
-            heapify(i, 0);
-        }
-    }
-    /**
-     * insert method.
-     *
-     * @param      item  The item
-     */
-    public void insert(final int item) {
-        array[size++] = item;
-        sort();
-    }
-    /**
-     * exchange function.
-     *
-     * @param      i     { parameter_description }
-     * @param      j     { parameter_description }
-     */
-    public void exchange(final int i, final int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    /**
-     * heapify method.
-     *
-     * @param      n     { parameter_description }
-     * @param      k     { parameter_description }
-     */
-    public void heapify(final int n, final int k) {
-        int largest = k;
-        int l = 2 * k + 1;
-        int r = 2 * k + 2;
-        if (l < n && array[l] > array[largest]) {
-            largest = l;
-        }
-        if (r < n && array[r] > array[largest]) {
-            largest = r;
-        }
-        if (largest != k) {
-            exchange(k, largest);
-            heapify(n, largest);
-        }
-    }
-    /**
-     * get item.
-     *
-     * @param      index  The index
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public int getitem(final int index) {
-        return array[index];
-    }
-    /**
-     * get size.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public int getsize() {
-        return size;
-    }
-}
-/**
- * Class for solution.
+ * class for solution.
  */
 public final class Solution {
     /**
@@ -112,20 +19,39 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        Heapsort heap =  new Heapsort(n);
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        MinPQ<Float> minpq = new MinPQ<Float>(n);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
+        float median = 0.0f;
         for (int i = 0; i < n; i++) {
-            heap.insert(Integer.parseInt(sc.nextLine()));
-            int size = heap.getsize();
-            if ((size) % 2 == 0) {
-                float median = ((float) heap.getitem((size - 1) / 2)
-                    + (float) heap.getitem(size / 2)) / 2;
-                System.out.println(median);
+            float k = scan.nextFloat();
+            if (k > median) {
+                minpq.insert(k);
             } else {
-                float median = (float) heap.getitem((size - 1) / 2);
+                maxpq.insert(k);
+            }
+            if (maxpq.size() - minpq.size() > 1) {
+                float x = maxpq.delMax();
+                minpq.insert(x);
+            }
+            if (minpq.size() - maxpq.size() > 1) {
+                float y = minpq.delMin();
+                maxpq.insert(y);
+            }
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max()) / 2;
                 System.out.println(median);
             }
+            if (maxpq.size() > minpq.size()) {
+                median = maxpq.max();
+                System.out.println(median);
+            }
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
+
         }
     }
 }
