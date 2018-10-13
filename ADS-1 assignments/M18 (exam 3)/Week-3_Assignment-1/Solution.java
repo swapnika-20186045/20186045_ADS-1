e283c3c8a8e63f5d4bf0774fbe26f6dd16c10fdd
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-13 10:47:06 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 assignments/M18 (exam 3)/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public final class Solution {
+	private Solution() {
+		//empty constructor.
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+		while(n > 0) {
+			int hrs = 6;
+			while(hrs > 0) {
+				String[] tokens = scan.nextLine().split(",");
+				System.out.println(tokens);
+				hrs--;
+			}
+			n--;
+		}
+	}
+}
