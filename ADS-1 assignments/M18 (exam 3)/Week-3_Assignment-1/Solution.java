17e4154c3c634dc93575e5da6f9fcac3aac5eb98
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-13 16:38:25 +0530

    "ads1_week3_exam: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/M18 (exam 3)/Week-3_Assignment-1/Solution.java
+++ ADS-1 assignments/M18 (exam 3)/Week-3_Assignment-1/Solution.java
@@ -14,16 +14,16 @@ class Stock implements Comparable<Stock> {
     /**
      * change in stock value.
      */
-    private double change;
+    private float change;
     /**
      * Constructs the object.
      *
      * @param      stocknamee  The stocknamee
      * @param      changee     The changee
      */
-    Stock(final String stocknamee, final String changee) {
+    Stock(final String stocknamee, final Float changee) {
         this.stockname = stocknamee;
-        this.change = Double.parseDouble(changee);
+        this.change = changee;
     }
     /**
      * Gets the name of the stock.
@@ -38,7 +38,7 @@ class Stock implements Comparable<Stock> {
      *
      * @return     The change.
      */
-    public double getChange() {
+    public Float getChange() {
         return this.change;
     }
     /**
@@ -98,56 +98,62 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-            BinarySearchST<String, Double> best = new  BinarySearchST<>();
-            BinarySearchST<String, Double> worst = new BinarySearchST<>();
+        BinarySearchST<String, Integer> best = new  BinarySearchST<>();
+        BinarySearchST<String, Integer> worst = new BinarySearchST<>();
         for (int i = 0; i < SIX; i++) {
             int count = 0;
             MinPQ<Stock> min = new MinPQ<>();
             MaxPQ<Stock> max = new MaxPQ<>();
             while (count < n) {
                 String[] tokens = scan.nextLine().split(",");
-                Stock stocks = new Stock(tokens[0], tokens[1]);
+                Stock stocks = new Stock(tokens[0], Float.parseFloat(tokens[1]));
                 min.insert(stocks);
                 max.insert(stocks);
                 count++;
             }
             for (int j = 0; j < FIVE; j++) {
                 Stock maxbest = max.delMax();
+                if (best.contains(maxbest.getStockName())) {
+                    int p = best.get(maxbest.getStockName());
+                    best.put(maxbest.getStockName(), ++p);
+                } else {
+                    best.put(maxbest.getStockName(), 1);
+                }
                 System.out.println(maxbest);
-                best.put(maxbest.getStockName(), maxbest.getChange());
             }
             System.out.println();
             for (int k = 0; k < FIVE; k++) {
                 Stock minworst = min.delMin();
+                if (worst.contains(minworst.getStockName())) {
+                    int p1 = worst.get(minworst.getStockName());
+                    worst.put(minworst.getStockName(), ++p1);
+                } else {
+                    worst.put(minworst.getStockName(), 1);
+                }
                 System.out.println(minworst);
-                worst.put(minworst.getStockName(), minworst.getChange());
             }
             System.out.println();
         }
         int num = Integer.parseInt(scan.nextLine());
-        while (num > 0) {
-            String[] tokens = scan.nextLine().split(",");
-            switch (tokens[0]) {
-            case "get" :
-                if (tokens[1].equals("MaxST")) {
-                    if (best.contains(tokens[2])) {
-                        System.out.println(best.get(tokens[2]));
-                    } else {
+        for (int i = 0; i < num; i++) {
+            String[] queries = scan.nextLine().split(",");
+            if (queries[0].equals("get")) {
+                if (queries[1].equals("maxST")) {
+                    if (!best.contains(queries[2])) {
                         System.out.println("0");
+                    } else {
+                        System.out.println(
+                            best.get(queries[2]));
                     }
-
                 } else {
-                    if (worst.contains(tokens[2])) {
-                        System.out.println(worst.get(tokens[2]));
-                    } else {
+                    if (!worst.contains(queries[2])) {
                         System.out.println("0");
+                    } else {
+                        System.out.println(
+                            worst.get(queries[2]));
                     }
                 }
-                break;
-                default :
-                break;
             }
-            num--;
         }
     }
 }
