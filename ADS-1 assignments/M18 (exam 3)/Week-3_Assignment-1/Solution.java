42a0561e47f0b230d3da302ab4bd9aeffa7db35a
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-13 12:08:13 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/M18 (exam 3)/Week-3_Assignment-1/Solution.java
+++ ADS-1 assignments/M18 (exam 3)/Week-3_Assignment-1/Solution.java
@@ -4,7 +4,76 @@
  */
 import java.util.Scanner;
 /**
- * class for Solution.
+ * Class for stock.
+ */
+class Stock implements Comparable<Stock> {
+    /**
+     * name of the stock.
+     */
+    private String stockname;
+    /**
+     * change in stock value.
+     */
+    private Double change;
+    /**
+     * Constructs the object.
+     *
+     * @param      stocknamee  The stocknamee
+     * @param      changee     The changee
+     */
+    Stock(final String stocknamee, final Double changee) {
+        this.stockname = stocknamee;
+        this.change = changee;
+    }
+    /**
+     * Gets the name of the stock.
+     *
+     * @return     The name.
+     */
+    public String getStockName() {
+        return this.stockname;
+    }
+    /**
+     * Gets the change in stock value.
+     *
+     * @return     The change.
+     */
+    public Double getChange() {
+        return this.change;
+    }
+    /**
+     * compares two objects.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Stock that) {
+        if (this.change > that.change) {
+            return 1;
+        }
+        if (this.change < that.change) {
+            return -1;
+        }
+        if (this.stockname.compareTo(that.stockname) > 0) {
+            return 1;
+        }
+        if (this.stockname.compareTo(that.stockname) < 0) {
+            return -1;
+        }
+        return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return this.stockname + " " + this.change;
+    }
+}
+/**
+ * Class for solution.
  */
 public final class Solution {
     /**
@@ -21,12 +90,31 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        BinarySearchST<String,Double> sixhrs = new BinarySearchST<>();
-        while (n > 0) {
-            String[] tokens = scan.nextLine().split(","); 
-            sixhrs.put(tokens[0],Double.parseDouble(tokens[1]));
-            System.out.println(sixhrs);
-            n--;
+        for (int i = 0; i < 6; i++) {
+            int count = 0;
+            MinPQ<Stock> min = new MinPQ<>();
+            MaxPQ<Stock> max = new MaxPQ<>();
+            while (count < n) {
+                String[] tokens = scan.nextLine().split(",");
+                Stock stocks = new Stock(tokens[0], Double.parseDouble(tokens[1]));
+                min.insert(stocks);
+                max.insert(stocks);
+                count++;
+            }
+            BinarySearchST<String, Double> best = new  BinarySearchST<>();
+            BinarySearchST<String, Double> worst = new BinarySearchST<>();
+            for (int j = 0; j < 5; j++) {
+                Stock maxbest = max.delMax();
+                System.out.println(maxbest);
+                best.put(maxbest.getStockName(), maxbest.getChange());
+            }
+            System.out.println();
+            for(int k = 0; k < 5; k++) {
+                Stock minworst = min.delMin();
+                System.out.println(minworst);
+                worst.put(minworst.getStockName(), minworst.getChange());
+            }
+            System.out.println();
         }
     }
 }
