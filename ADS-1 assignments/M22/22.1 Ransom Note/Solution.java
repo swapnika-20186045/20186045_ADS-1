ed5cda6e365c0a5c508f9c97600827081416c9db
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-25 17:47:50 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 assignments/M22/22.1 Ransom Note/Solution.java
@@ -0,0 +1,363 @@
+/**
+ * Ransom Note.
+ */
+import java.util.Scanner;
+import java.lang.Iterable;
+import java.util.Iterator;
+/**
+ * Class for sequential search st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
+class SequentialSearchST<Key, Value> {
+    /**
+     * number of key-value pairs.
+     */
+    private int n;
+    /**
+     * the linked list of key-value pairs.
+     */
+    private Node first;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * declaration of variable.
+         */
+        private Key key;
+        /**
+         * declaration of variable.
+         */
+        private Value val;
+        /**
+         * declaration of variable.
+         */
+        private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      keyy   The key
+         * @param      vall   The value
+         * @param      nextt  The next
+         */
+        public Node(final Key keyy, final Value vall, final Node nextt)  {
+            this.key  = keyy;
+            this.val  = vall;
+            this.next = nextt;
+        }
+    }
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SequentialSearchST() {
+        //empty constructor.
+    }
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+    /**
+     * Is this symbol table empty?
+     * @return {@code true} if this symbol table is empty and
+     * {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *     {@code false} otherwise
+     */
+    public boolean contains(final Key key) {
+        return get(key) != null;
+    }
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in
+     * the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     */
+    public Value get(final Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the
+     * old value with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the
+     * symbol table.
+     * @param key the key
+     * @param val the value
+     */
+    public void put(final Key key, final Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        n++;
+    }
+
+    /**
+     * Removes the key and associated value from the symbol table
+     * (if the key is in the symbol table).
+     * @param key the key
+     */
+    public void delete(final Key key) {
+        first = delete(first, key);
+    }
+
+    // delete key in linked list beginning at Node x
+    // warning: function call stack too large if table is large
+    private Node delete(final Node x, final Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     * @return all keys in the symbol table as an {@code Iterable}
+     */
+    public Iterable<Key> keys()  {
+        Queue<Key> queue = new Queue<Key>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
+    }
+}
+/**
+ * Class for separate chaining hash st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
+class SeparateChainingHashST<Key, Value> {
+    /**
+     * declaration of variable.
+     */
+    private static final int INIT_CAPACITY = 4;
+    /**
+     * number of key-value pairs.
+     */
+    private int n;
+    /**
+     * hash table size.
+     */
+    private int m;
+    /**
+     * array of linked-list symbol tables.
+     */
+    private SequentialSearchST<Key, Value>[] st;
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    }
+    /**
+     * Initializes an empty symbol table with {@code m} chains.
+     * @param m the initial number of chains
+     */
+    public SeparateChainingHashST(final int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++)
+            st[i] = new SequentialSearchST<Key, Value>();
+    } 
+    // resize the hash table to have the given number of chains,
+    // rehashing all of the keys
+    private void resize(final int chains) {
+        SeparateChainingHashST<Key, Value> temp = new
+        SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m  = temp.m;
+        this.n  = temp.n;
+        this.st = temp.st;
+    }
+
+    // hash value between 0 and m-1
+    private int hash(final Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    } 
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    } 
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(final Key key) {
+        if (key == null) throw new IllegalArgumentException(
+            "argument to contains() is null");
+        return get(key) != null;
+    } 
+
+    /**
+     * Returns the value associated with the specified key in this symbol
+     * table.
+     *
+     * @param  key the key
+     * @return the value associated with {@code key} in the symbol table;
+     *         {@code null} if no such value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(final Key key) {
+        if (key == null) throw new IllegalArgumentException("key is null");
+        int i = hash(key);
+        return st[i].get(key);
+    } 
+
+    /**
+     * Inserts the specified key-value pair into the symbol table,
+     * overwriting the old value with the new value if the symbol table
+     * already contains the specified key. Deletes the specified key
+     * (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(final Key key, final Value val) {
+        if (key == null) throw new IllegalArgumentException(
+            "first argument to put() is null");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if average length of list >= 10
+        if (n >= 10*m) resize(2*m);
+
+        int i = hash(key);
+        if (!st[i].contains(key)) n++;
+        st[i].put(key, val);
+    } 
+
+    /**
+     * Removes the specified key and its associated value from this symbol
+     * table    
+     * (if the key is in this symbol table).    
+     *
+     * @param  key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(final Key key) {
+        if (key == null) throw new IllegalArgumentException(
+            "argument to delete() is null");
+
+        int i = hash(key);
+        if (st[i].contains(key)) n--;
+        st[i].delete(key);
+
+        // halve table size if average length of list <= 2
+        if (m > INIT_CAPACITY && n <= 2*m) resize(m/2);
+    }
+    /**
+     * return keys in symbol table as an Iterable.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
+    }
+}
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty constructor.
+    }
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        SeparateChainingHashST<String, Integer> hash = new
+                    SeparateChainingHashST<String, Integer>();
+        String integer = scan.nextLine();
+        String[] arr = scan.nextLine().split(" ");
+        for(int i = 0; i < arr.length; i++) {
+            if (hash.contains(arr[i])) {
+                hash.put(arr[i], hash.get(arr[i]) + 1); 
+            } else {
+                hash.put(arr[i], 1);
+            }
+        }
+
+        String[] strarr = scan.nextLine().split(" ");
+        for (int i = 0; i < strarr.length; i++) {
+            if (hash.contains(strarr[i]) ) {
+                if(hash.get(strarr[i]) == 0) {
+                    System.out.print("No");
+                    return;
+                } else {
+                    hash.put(strarr[i], hash.get(strarr[i]) - 1);
+                }
+            } else {
+                System.out.print("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
